9f2f6d924d0d9705d393aac5298e55355ea5ea4a
Dini Auliya Fauziah <diniauliyaf@gmail.com>
2025-04-08 20:49:50 +0700

    Membuat kolom input

--- app/src/main/java/com/diniauliya0015/assesment1mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/diniauliya0015/assesment1mobpro/ui/screen/MainScreen.kt
@@ -4,16 +4,19 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -29,6 +32,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -66,6 +71,9 @@ fun ScreenContent(modifier: Modifier = Modifier){
     )
     var selectedText by remember { mutableStateOf(list[0]) }
     var isExpanded by remember { mutableStateOf(false) }
+    var daya by remember { mutableStateOf("") }
+    var energi by remember { mutableStateOf("") }
+    var waktu by remember { mutableStateOf("") }
     Column (
         modifier = modifier
             .fillMaxSize()
@@ -119,6 +127,139 @@ fun ScreenContent(modifier: Modifier = Modifier){
                                 isExpanded = false
                             },
                             contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                        )
+                    }
+                }
+            }
+            when (selectedText) {
+                stringResource(R.string.hitung_daya) -> {
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ){
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = energi,
+                            onValueChange = { energi = it },
+                            label = { Text(text = stringResource(R.string.energi)) },
+                            trailingIcon = {
+                                Text(
+                                    text = stringResource(R.string.satuan_energi),
+                                    fontSize = 12.sp
+                                )
+                            },
+                            textStyle = TextStyle(fontSize = 14.sp),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = waktu,
+                            onValueChange = { waktu = it },
+                            label = { Text(text = stringResource(R.string.waktu)) },
+                            trailingIcon = {
+                                Text(
+                                    text = stringResource(R.string.satuan_waktu),
+                                    fontSize = 12.sp
+                                )
+                            },
+                            textStyle = TextStyle(fontSize = 14.sp),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Done
+                            )
+                        )
+                    }
+                }
+                stringResource(R.string.hitung_energi) -> {
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = daya,
+                            onValueChange = { daya = it },
+                            label = { Text(text = stringResource(R.string.daya)) },
+                            trailingIcon = {
+                                Text(
+                                    text = stringResource(R.string.satuan_daya),
+                                    fontSize = 12.sp
+                                )
+                            },
+                            textStyle = TextStyle(fontSize = 14.sp),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = waktu,
+                            onValueChange = { waktu = it },
+                            label = { Text(text = stringResource(R.string.waktu)) },
+                            trailingIcon = {
+                                Text(
+                                    text = stringResource(R.string.satuan_waktu),
+                                    fontSize = 12.sp
+                                )
+                            },
+                            textStyle = TextStyle(fontSize = 14.sp),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Done
+                            )
+                        )
+                    }
+                }
+                stringResource(R.string.hitung_waktu) -> {
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ){
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = daya,
+                            onValueChange = { daya = it },
+                            label = { Text(text = stringResource(R.string.daya)) },
+                            trailingIcon = {
+                                Text(
+                                    text = stringResource(R.string.satuan_daya),
+                                    fontSize = 12.sp
+                                )
+                            },
+                            textStyle = TextStyle(fontSize = 14.sp),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = energi,
+                            onValueChange = { energi = it },
+                            label = { Text(text = stringResource(R.string.energi)) },
+                            trailingIcon = {
+                                Text(
+                                    text = stringResource(R.string.satuan_energi),
+                                    fontSize = 12.sp
+                                )
+                            },
+                            textStyle = TextStyle(fontSize = 14.sp),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Done
+                            )
                         )
                     }
                 }

b79cd5cd92ee70e36b3d5ff01ad0b40ae5f2cc60
Dini Auliya Fauziah <diniauliyaf@gmail.com>
2025-04-08 20:15:14 +0700

    Membuat dropdown

--- app/src/main/java/com/diniauliya0015/assesment1mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/diniauliya0015/assesment1mobpro/ui/screen/MainScreen.kt
@@ -5,21 +5,33 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.diniauliya0015.assesment1mobpro.R
 import com.diniauliya0015.assesment1mobpro.ui.theme.Assesment1MobproTheme
 
@@ -43,9 +55,17 @@ fun MainScreen(){
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
     val gambarAwal = painterResource(R.drawable.segitiga_rumus)
+    val list = listOf(
+        stringResource(R.string.hitung_daya),
+        stringResource(R.string.hitung_energi),
+        stringResource(R.string.hitung_waktu)
+    )
+    var selectedText by remember { mutableStateOf(list[0]) }
+    var isExpanded by remember { mutableStateOf(false) }
     Column (
         modifier = modifier
             .fillMaxSize()
@@ -64,8 +84,47 @@ fun ScreenContent(modifier: Modifier = Modifier){
             text = stringResource(id = R.string.app_intro),
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
         )
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            ExposedDropdownMenuBox(
+                expanded = isExpanded,
+                onExpandedChange = { isExpanded = !isExpanded}
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .width(220.dp)
+                        .menuAnchor(),
+                    value = selectedText,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text(text = stringResource(R.string.pilih_rumus)) },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)  },
+                    textStyle = TextStyle(fontSize = 16.sp),
+                    singleLine = true
+                )
+                ExposedDropdownMenu(
+                    expanded = isExpanded,
+                    onDismissRequest = { isExpanded = false }) {
+                    list.forEachIndexed { index, text ->
+                        DropdownMenuItem(
+                            text = { Text(text = text) },
+                            onClick = {
+                                selectedText = list[index]
+                                isExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                        )
+                    }
+                }
+            }
+
+        }
     }
 }
 

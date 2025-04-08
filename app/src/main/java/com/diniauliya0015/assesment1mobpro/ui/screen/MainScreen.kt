16ef79ae276f496b6e04fe1d0a9bd2141ddd13ce
Dini Auliya Fauziah <diniauliyaf@gmail.com>
2025-04-08 19:44:00 +0700

    Menambahkan gambar dan menambahkan intro teks

--- app/src/main/java/com/diniauliya0015/assesment1mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/diniauliya0015/assesment1mobpro/ui/screen/MainScreen.kt
@@ -1,7 +1,12 @@
 package com.diniauliya0015.assesment1mobpro.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,9 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.diniauliya0015.assesment1mobpro.R
 import com.diniauliya0015.assesment1mobpro.ui.theme.Assesment1MobproTheme
 
@@ -37,10 +45,28 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android",
+    val gambarAwal = painterResource(R.drawable.segitiga_rumus)
+    Column (
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        Image(
+            painter = gambarAwal,
+            contentDescription = stringResource(R.string.gambar_awal),
+            modifier = modifier
+                .size(230.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(id = R.string.app_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
 }
 
 @Preview(showBackground = true)
